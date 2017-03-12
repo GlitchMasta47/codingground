fe7e8bd4e64a6138bfcc279b2a0d35e506ea4992
GlitchMasta47 <glitchmasta47@hotmail.com>
2017-03-12 17:54:45 -0500

    Update main.js

--- New Project-20170312/main.js
+++ New Project-20170312/main.js
@@ -1,1 +1,10 @@
 /* Node.js Express server with Bulma */
+var colors = require("colors") // Colored console logs
+colors.setTheme({
+  info: ["bgGreen","white"],
+  warn: ["bgYellow","white"]
+}) // Set info and warn presets
+var express = require("express") // Express server module
+var server = express() // Initialize server
+var bulma = require("bulma") // Even though Bulma isn't an "interactive module," this will remind the user to install the bulma module if it isn't already installed
+
